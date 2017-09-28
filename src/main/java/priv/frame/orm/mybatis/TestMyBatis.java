fc08e8135302e10d203418359b60a38654287c55
ZhangKai <zhangkai600148@pwrd.com>
2017-09-28 16:10:08 +0800

    add new Config file read mode

--- src/main/java/priv/frame/orm/mybatis/TestMyBatis.java
+++ src/main/java/priv/frame/orm/mybatis/TestMyBatis.java
@@ -1,30 +1,53 @@
 package priv.frame.orm.mybatis;
 
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 /**
  * Created by zhangkai on 2017/9/27.
  */
 public class TestMyBatis {
-    public static void main(String[] argvs)
+    public static void main(String[] argvs) {
+        test1();
+        test2();
+    }
+
+    public static void test1()
     {
         String resource = "mybatis/mybatis_conf.xml";
 
+        InputStream is = TestMyBatis.class.getClassLoader().getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession session = sqlSessionFactory.openSession();
 
-            InputStream is = TestMyBatis.class.getClassLoader().getResourceAsStream(resource);
-            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
-            SqlSession session = sqlSessionFactory.openSession();
+        String statement = "priv.frame.orm.mybatis.userMapper.getUser";
+
+        User user = session.selectOne(statement, 1);
+        System.out.println(user);
+    }
+
+    public static void test2()
+    {
+        String resource = "mybatis/mybatis_conf.xml";
+
+        try {
+            Reader reader = Resources.getResourceAsReader(resource);
+            
+            SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            SqlSession session = sessionFactory.openSession();
 
             String statement = "priv.frame.orm.mybatis.userMapper.getUser";
 
             User user = session.selectOne(statement, 1);
             System.out.println(user);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
